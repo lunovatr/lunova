1c4644c2245306540246c387b32353f78e3de587
ksyusuf <ksyusuf85@gmail.com>
2025-08-30 01:32:55 +0300

    port update lokalde çalışırken port çakışımaması için.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5174, // Client frontend için farklı port
+  },
   plugins: [
     react(),
     svgr({
